ace0884faa2d51b6abe33587368dd9502434dfc1
swhan-cubox-ai <swhan@cubox.ai>
2022-04-28 18:28:20 +0900

    API

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,30 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+     public String hello(Model model ){
+         model.addAttribute("data", "hello!!");
+         return "hello";
+     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvcModel(@RequestParam(value = "name",required = true) String name, Model model) {
+        model.addAttribute("name", name);
+    return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloMvcString(@RequestParam(value = "name",required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-string";
+    }
+}
